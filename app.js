c4a8684675aba8407dafecddb6fa3426198784f7
racheal nakalawa <naksbals@gmail.com>
2022-04-05 14:12:42 +0300

    Add routing for authentication forms and parking

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const portNumber = 3000
 
 //setting the view engine
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
 
 //setting directory for static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,12 +20,32 @@ app.use(morgan('dev'));
 //middleware
 
 // routing
+
 app.get('/', (req, res) => {
 	res.render('authenticationForms/login')
 })
 
+app.get("/login", (req, res) => {
+	res.redirect('/');
+})
+
+app.get("/signup", (req, res) => {
+	res.render('authenticationForms/signup');
+})
+
 app.get('/home', (req, res) => {
 	res.render('Home')
+})
+
+app.get('/parkings', (req, res)=> {
+	res.render('parking/ParkingHome')
+})
+
+
+// 404
+
+app.use((req, res) => {
+	res.status(404).render('404');
 })
 
 //listen for requests
