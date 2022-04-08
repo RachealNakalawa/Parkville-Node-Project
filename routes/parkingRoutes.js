77f4b53ea23d0226ecab343f78bbdd90b8212d6e
racheal nakalawa <naksbals@gmail.com>
2022-04-08 14:53:35 +0300

    Create routing for editing an existing car

--- routes/parkingRoutes.js
+++ routes/parkingRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/home', parkingController.homePage)
 router.get('/parkings', parkingController.parkingDashboard)
 router.post('/parkings', parkingController.registerCar)
-router.get('/edit/:id', parkingController.editParkedCar);
+router.get('/edit/:id', parkingController.editCarForm);
+router.post('/edit', parkingController.editParkedCar);
 
 module.exports = router;
