c0e32f11b09c27e62af37b5856d97159e1fd18aa
racheal nakalawa <naksbals@gmail.com>
2022-04-10 16:40:47 +0300

    Implement logging in

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const User = require('../models/userModel');
 const router = express.Router();
+const passport = require('passport');
 
+
+// login a user
 router.get('/', (req, res) => {
 	res.render('authenticationForms/login')
 })
-
+// ---- incase someone uses the resource /login redirect them to /
 router.get("/login", (req, res) => {
 	res.redirect('/');
 })
 
+// passport provides the controller to handle loging in. So it will authenticate using the strategy it takes in, and respond or react based on the options it takes in as well. 
+router.post('/login', passport.authenticate('local', {
+	failureRedirect: "/",
+	successRedirect: '/home'
+}))
+
+
+// signup a user
 router.get("/signup", (req, res) => {
 	//just to prove that you are simply calling a file
 	res.render('authenticationForms/signup.ejs');
