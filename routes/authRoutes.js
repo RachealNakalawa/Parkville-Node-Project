bf052b96b3895e8f9ab753ff04b72ae303c9e030
racheal nakalawa <naksbals@gmail.com>
2022-04-10 15:48:18 +0300

    Implement signing up of users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models/userModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -12,6 +13,26 @@ router.get("/login", (req, res) => {
 router.get("/signup", (req, res) => {
 	//just to prove that you are simply calling a file
 	res.render('authenticationForms/signup.ejs');
+})
+
+router.post("/signup", async (req, res) => {
+	try {
+		let userExistance = await User.findOne({email: req.body.email});
+		if(userExistance) {
+			res.send("Email already exists");
+		}else {
+			let newUser = new User(req.body);
+			await User.register(newUser, req.body.password, (err) => {
+				if(err) {
+					res.send('We could create a user, Please try again later');
+				}else{
+					res.redirect('/');
+				}
+			})
+		}
+	}catch(err) {
+		console.error(err)
+	}
 })
 
 module.exports = router;
