96aae661795031d9d3a66df2dc5b324c9fd1a295
racheal nakalawa <naksbals@gmail.com>
2022-04-10 23:16:36 +0300

    Update signup
    Add some comments

--- routes/parkingRoutes.js
+++ routes/parkingRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+// const connectEnsureLoggedIn = require('connect-ensure-login');
 //The commented line below does something called "Destructuring" in javascript
 // const { homePage, parkingDashboard } = require('../controllers/parkingController');
 const parkingController = require('../controllers/parkingController');
 const router = express.Router();
 
-
+// router.get('/home',connectEnsureLoggedIn.ensureLoggedIn("/login"), parkingController.homePage)
 router.get('/home', parkingController.homePage)
 router.get('/parkings', parkingController.parkingDashboard)
 router.post('/parkings', parkingController.registerCar)
