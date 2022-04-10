42b5a18e0fe60da8bc2038c7c43f9b5af93f2e72
racheal nakalawa <naksbals@gmail.com>
2022-04-10 16:43:29 +0300

    Implement using session and passport

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const morgan = require('morgan');
 const expressLayouts = require('express-layout');
 const mongoose =  require('mongoose');
 const methodoverride = require('method-override');
+const session = require('express-session');
+const passport = require('passport');
 
 
 const authRoutes = require('./routes/authRoutes');
 const parkingRoutes = require('./routes/parkingRoutes');
 const config = require('./config/database_config');
+const User = require('./models/userModel');
 
 //connecting to the database
 mongoose.connect(config.database);
@@ -35,10 +38,27 @@ app.set("layouts", path.join(__dirname, "/views/layouts"));
 //middleware
 //setting directory for static files
 app.use(express.static(path.join(__dirname, "public")));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(expressLayouts());
 app.use(express.urlencoded({extended: true}));
 app.use(methodoverride('_method'));
+app.use(session({
+	secret: 'rashiyeParkville',
+	resave: false,
+	saveUninitialized: false,
+	// cookie: {maxAge: 60 * 60 * 1000}
+}))
+
+app.use(passport.initialize());
+//connect passpport to session which is express-session
+app.use(passport.session());
+// tell it the strategy which is going to use the help of passport-local-mongoose
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 // routing
 
